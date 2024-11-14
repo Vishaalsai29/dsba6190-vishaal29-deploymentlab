3a7bbd5dfc97e01e705f5e16303b7ccf5d122a0a
Vishaalsai29 <vishaalsai29@gmail.com>
2024-11-13 19:42:49 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -37,7 +37,7 @@ variable "environment" {
 variable "student_name" {
   description = "Application Name"
   type        = string
-  default     = "vishaal29" 
+  default     = "vishaal29"
 }
 
 variable "class_name" {
