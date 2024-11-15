e64e525692a30f1c3e7f1ad81a72bd01fe335dc5
Vishaalsai29 <vishaalsai29@gmail.com>
2024-11-14 19:26:40 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -83,7 +83,7 @@ resource "azurerm_mssql_database" "sdb" {
   collation    = "SQL_Latin1_General_CP1_CI_AS"
   license_type = "LicenseIncluded"
   max_size_gb  = 2
-  sku_name     = "Standard"
+  sku_name     = "S0"
   enclave_type = "VBS"
 
   tags = {
