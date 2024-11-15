6aa8e0f5293b640f9abb669a7dd9dc6da4cc83e4
Vishaalsai29 <vishaalsai29@gmail.com>
2024-11-14 20:03:25 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -97,8 +97,8 @@ resource "azurerm_mssql_database" "sdb" {
 }
 
 resource "azurerm_mssql_virtual_network_rule" "snr" {
-  name                           = "snr-${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
-  server_id                      = azurerm_mssql_server.sser.id
-  subnet_id                      = azurerm_subnet.snet.id
+  name         = "snr-${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
+  server_id    = azurerm_mssql_server.sser.id
+  subnet_id    = azurerm_subnet.snet.id
   ignore_missing_vnet_service_endpoint = false
 }
